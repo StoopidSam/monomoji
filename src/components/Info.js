5f7729eefa25f68373227327c2f731bc3fdb098d
Sam Cutter <samuelcutter27@gmail.com>
2022-11-14 15:15:35 +0000

    Added info section

---
+++ src/components/Info.js
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+export default function Info() {
+  return (
+    <section>
+      <h2>Info</h2>
+      <ul>
+        <li>
+          <p>
+            Monomoji is a web app that creates custom card decks for games that
+            are similar to "Dobble" or "Spot it!". Select the amount of cards
+            you'd like, pick some emojis, and click "Create Deck" to get
+            started.
+          </p>
+        </li>
+        <li>
+          <p>
+            To view my source code, visit me on{" "}
+            <Link href="https://github.com/StoopidSam/monomoji">GitHub</Link>.
+          </p>
+        </li>
+      </ul>
+    </section>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,27 @@
 import Header from "../components/Header";
+import Info from "../components/Info";
+import Head from "next/head";
 
 export default function HomePage() {
   return (
     <div>
+      <Head>
+        <title>Monomoji</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossorigin
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
       <Header />
+      <main>
+        <Info />
+      </main>
     </div>
   );
 }
