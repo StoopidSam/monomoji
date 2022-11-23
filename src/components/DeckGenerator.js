4561c961a2d197aa64734f46a5f3b5a5ffd4d159
Sam Cutter <samuelcutter27@gmail.com>
2022-11-23 22:33:00 +0000

    Added basic submit functionality, which generates cards.

--- src/components/DeckGenerator.js
+++ src/components/DeckGenerator.js
@@ -1,11 +1,15 @@
 import { useState } from "react";
 import NumOfCardsPicker from "./NumOfCardsPicker";
 import EmojisPicker from "./EmojisPicker";
+import SubmitButton from "./SubmitButton";
+import { generate_cards } from "../generate_cards";
 
 export default function DeckGenerator() {
   const [numOfCards, setNumOfCards] = useState("(select one)");
 
   const [emojis, setEmojis] = useState([]);
+
+  const [cards, setCards] = useState([]);
 
   function handleNumOfCardsChange(numOfCards) {
     setNumOfCards(parseInt(numOfCards));
@@ -21,7 +25,6 @@ export default function DeckGenerator() {
     setEmojis(emptyEmojisList);
   }
 
-  // Define a function that updates a single emoji in the emojis list
   function handleEmojiChange(emoji, index) {
     let newEmojisList = [...emojis];
 
@@ -30,13 +33,23 @@ export default function DeckGenerator() {
     setEmojis(newEmojisList);
   }
 
+  function handleFormSubmit(event) {
+    event.preventDefault();
+
+    let emoji_chars = [];
+
+    for (let emoji of emojis) {
+      emoji_chars.push(emoji.emoji_char);
+    }
+
+    setCards(generate_cards(2, emoji_chars));
+  }
+
   return (
     <section>
       <h2>Deck Generator</h2>
-      <p>
-        This is the section where you get to generate your custom card deck.
-      </p>
-      <form>
+      <p>This is where you get to generate your own custom card deck.</p>
+      <form onSubmit={handleFormSubmit}>
         <NumOfCardsPicker
           handleNumOfCardsChange={handleNumOfCardsChange}
           numOfCards={numOfCards}
@@ -46,6 +59,7 @@ export default function DeckGenerator() {
           handleEmojiChange={handleEmojiChange}
           numOfCards={numOfCards}
         />
+        <SubmitButton />
       </form>
     </section>
   );
