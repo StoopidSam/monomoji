a091e09892e8ff6e57c6a7d141c30bbe96c53b11
Sam Cutter <samuelcutter27@gmail.com>
2022-12-01 22:23:39 +0000

    Styled randomize button

--- src/components/Randomize.js
+++ src/components/Randomize.js
@@ -1,7 +1,7 @@
 export default function Randomize({ randomizeEmojis }) {
   return (
-    <button type="button" onClick={randomizeEmojis}>
-      Randomize
+    <button id="Randomize" type="button" onClick={randomizeEmojis}>
+      RANDOMIZE 🔀
     </button>
   );
 }
